290758eb8e710bd6344371f28c8b7479d2e30b89
Axiomatic314 <rumkate289@gmail.com>
2024-11-22 16:19:54 +1300

    removed annoying files

--- external/JASSv2/external/zstd/lib/CMakeFiles/libzstd_static.dir/__/src/zstd/lib/common/error_private.c.o.d
+++
@@ -1,6 +0,0 @@
-lib/CMakeFiles/libzstd_static.dir/__/src/zstd/lib/common/error_private.c.o: \
- /home/harka424/Documents/JASSv2/external/zstd/src/zstd/lib/common/error_private.c \
- /usr/include/stdc-predef.h \
- /home/harka424/Documents/JASSv2/external/zstd/src/zstd/lib/common/error_private.h \
- /usr/lib/gcc/x86_64-linux-gnu/11/include/stddef.h \
- /home/harka424/Documents/JASSv2/external/zstd/src/zstd/lib/common/zstd_errors.h
