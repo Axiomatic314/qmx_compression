3f29a5af6fea8baed91bd7d5db28cee1668350f4
Axiomatic314 <rumkate289@gmail.com>
2024-11-26 16:48:01 +1300

    finally working

--- build.rs
+++ build.rs
@@ -1,23 +1,20 @@
-extern crate cmake;
+
 
 fn main() {
-    // // println!("cargo:rustc-link-search=external/JASSv2/build/source/", );
-    // // println!("cargo:rustc-link-lib=static=JASSlib");
-    // println!("cargo:rustc-link-lib=pain");
-
-    // cc::Build::new()
-    //     .file("pain.cpp")
-    //     .cpp(true)
-    //     .compile("libpain.a");
-
-    // cc::Build::new()
-    //     // .file("src/jass.cpp")
-    //     .file("src/compress_integer_qmx_improved.cpp")
-    //     .cpp(true)
-    //     .compile("libjass.a");
-
-    let dst = cmake::build("libJASS");
-    println!("cargo:rustc-link-search=native={}", dst.display());
-    println!("cargo:rustc-link-lib=static=JASS");
+    
+    cc::Build::new()
+        .file("src/jass.cpp")
+        .file("src/compress_integer_qmx_improved.h")
+        .file("src/compress_integer_qmx_improved.cpp")
+        .cpp(true)
+        .flag("-fPIC")
+        .flag("-D_GLIBCXX_USE_CXX11_ABI=1")
+        .flag("-Wno-implicit-fallthrough")
+        .flag("-Wno-unused-parameter")
+        .flag("-g")
+        .flag("-march=native")
+        .flag("-mbmi")
+        .flag("-mavx2")
+        .compile("libjass.a");
 
 }
